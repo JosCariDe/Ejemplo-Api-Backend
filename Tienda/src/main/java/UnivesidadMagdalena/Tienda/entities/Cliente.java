18106a92fd9b4d7fffc9c4473f5cd30d797f508a
jose <josenarvaezcariaga@gmail.com>
2024-03-26 17:59:52 -0500

    Agregando la relacion entre cliente a pedidos Un cliente puede tener varios pedidos

--- Tienda/src/main/java/UnivesidadMagdalena/Tienda/entities/Cliente.java
+++ Tienda/src/main/java/UnivesidadMagdalena/Tienda/entities/Cliente.java
@@ -1,9 +1,12 @@
 package UnivesidadMagdalena.Tienda.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Entity
 @Table(name = "clientes")
@@ -11,4 +14,7 @@ public class Cliente extends BaseEntity{
     private String nombre;
     private String email;
     private String direccion;
+
+    @OneToMany(mappedBy = "cliente")
+    private List<Pedido> pedidos;
 }
--- Tienda/src/main/java/UnivesidadMagdalena/Tienda/entities/Pedido.java
+++ Tienda/src/main/java/UnivesidadMagdalena/Tienda/entities/Pedido.java
@@ -2,6 +2,8 @@ package UnivesidadMagdalena.Tienda.entities;
 
 import UnivesidadMagdalena.Tienda.entities.enumClass.Status;
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 import java.time.LocalDateTime;
@@ -9,6 +11,9 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "pedidos")
 public class Pedido extends BaseEntity{
+
+    @ManyToOne
+    @JoinColumn(name = "id_cliente", nullable = false)
     private Cliente cliente;
     private LocalDateTime fechaPedido;
     private Status status;
