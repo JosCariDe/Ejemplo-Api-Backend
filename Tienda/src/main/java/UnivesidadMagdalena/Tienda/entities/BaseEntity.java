f5c12645e74a53e53511251c0a2357b3aacbdede
JosCariDe <jdnarvaez@unimagdalena.edu.co>
2024-03-27 10:44:43 -0500

    Se agregó @Builder y se cambió la clase a abstracta

--- Tienda/src/main/java/UnivesidadMagdalena/Tienda/entities/BaseEntity.java
+++ Tienda/src/main/java/UnivesidadMagdalena/Tienda/entities/BaseEntity.java
@@ -4,10 +4,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.io.Serializable;
 
@@ -16,7 +13,8 @@ import java.io.Serializable;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class BaseEntity implements Serializable {
+@Builder
+public abstract class BaseEntity implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
