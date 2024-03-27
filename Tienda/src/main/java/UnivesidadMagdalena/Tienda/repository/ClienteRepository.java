6f312ecf97ea18b4aafbf42f3f15e4e8664f5c43
JosCariDe <jdnarvaez@unimagdalena.edu.co>
2024-03-27 14:05:56 -0500

    Se agregaron los 3 metodos de busqueda en la capa de persistencia de cliente Encontrar clientes por email Encontrar clientes por dirección Encontrar clientes por todos los clientes que comiencen por un nombre

--- Tienda/src/main/java/UnivesidadMagdalena/Tienda/repository/ClienteRepository.java
+++ Tienda/src/main/java/UnivesidadMagdalena/Tienda/repository/ClienteRepository.java
@@ -2,7 +2,14 @@ package UnivesidadMagdalena.Tienda.repository;
 
 import UnivesidadMagdalena.Tienda.entities.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-
+    List<Cliente> findByEmail(String email);
+    List<Cliente> findByDireccion(String direccion);
+    @Query("SELECT c FROM Cliente p WHERE c.nombre LIKE :term%")
+    List<Cliente> buscarPorComienzoNombreCon(@Param("term") String nombre);
 }
