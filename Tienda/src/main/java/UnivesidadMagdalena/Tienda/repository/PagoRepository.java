82c7ab13b0897ddd93ef3000879d40c0f1593a4b
JosCariDe <jdnarvaez@unimagdalena.edu.co>
2024-03-27 14:27:13 -0500

    Se agregaron las 2 funciones a la capa de persistencia de la entidad de Pago  Recuperar pagos dentro de un rango de fecha
    Recuperar pagos por un identificador de una orden y método de pago

--- Tienda/src/main/java/UnivesidadMagdalena/Tienda/repository/PagoRepository.java
+++ Tienda/src/main/java/UnivesidadMagdalena/Tienda/repository/PagoRepository.java
@@ -1,8 +1,13 @@
 package UnivesidadMagdalena.Tienda.repository;
 
 import UnivesidadMagdalena.Tienda.entities.Pago;
+import UnivesidadMagdalena.Tienda.enumClass.MetodoPago;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PagoRepository extends JpaRepository<Pago,Long> {
+import java.time.LocalDateTime;
+import java.util.List;
 
+public interface PagoRepository extends JpaRepository<Pago,Long> {
+    List<Pago> findByFechaPagoBetween(LocalDateTime fechaInicio, LocalDateTime fechaFin);
+    List<Pago> findByIdAndMetodoPago(Long id, MetodoPago metodoPago);
 }
