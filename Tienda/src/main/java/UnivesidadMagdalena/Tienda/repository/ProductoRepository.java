b612ecaad9f962d244cb5b6d7c618d4cc8aeb361
JosCariDe <jdnarvaez@unimagdalena.edu.co>
2024-03-27 13:44:19 -0500

    Agregado los 3 metodos de busqueda en la capa de persistencia de producto Buscar productos según un término de búsqueda Buscar los productos que están en stock.
    Buscar los productos que no superen un precio y un stock determinado

--- Tienda/src/main/java/UnivesidadMagdalena/Tienda/repository/ProductoRepository.java
+++ Tienda/src/main/java/UnivesidadMagdalena/Tienda/repository/ProductoRepository.java
@@ -2,7 +2,17 @@ package UnivesidadMagdalena.Tienda.repository;
 
 import UnivesidadMagdalena.Tienda.entities.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProductoRepository extends JpaRepository<Producto,Long> {
+    @Query("SELECT p FROM Producto p WHERE p.nombre LIKE %:term%")
+    List<Producto> buscarPorTerminoDeBusqueda(@Param("term") String term);
 
+    List<Producto> findByStock(Integer cantidad);
+
+    @Query("SELECT p FROM Producto p WHERE p.price <= :precioMaximo AND p.stock <= :stockMaximo")
+    List<Producto> buscarPorPrecioMaximoYStockMaximo(@Param("precioMaximo") Integer precioMaximo, @Param("stockMaximo") Integer stockMaximo);
 }
