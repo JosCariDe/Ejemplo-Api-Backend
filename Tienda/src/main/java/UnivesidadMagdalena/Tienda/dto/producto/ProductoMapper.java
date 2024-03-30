1f5a7095ffe07524147a262ad7d9168fa3a82432
JosCariDe <jdnarvaez@unimagdalena.edu.co>
2024-03-30 12:13:05 -0500

    Se agregó el mapper de productos

--- Tienda/src/main/java/UnivesidadMagdalena/Tienda/dto/producto/ProductoMapper.java
+++ Tienda/src/main/java/UnivesidadMagdalena/Tienda/dto/producto/ProductoMapper.java
@@ -1,4 +1,18 @@
 package UnivesidadMagdalena.Tienda.dto.producto;
 
+import UnivesidadMagdalena.Tienda.entities.Producto;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+
+import java.util.List;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface ProductoMapper {
+
+    ProductoDto productoToProductoDto(Producto producto);
+    Producto productoDtoToProducto(ProductoDto productoDto);
+
+    List<ProductoDto> productosToProductosDto(List<Producto> productoList);
+    List<Producto> productosDtoToProductos(List<ProductoDto> productoDtoList);
+
 }
