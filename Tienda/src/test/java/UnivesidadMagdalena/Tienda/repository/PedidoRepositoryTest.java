bbf01f3e5eec408297f03bd72a1d525aa9d873cf
jose <josenarvaezcariaga@gmail.com>
2024-03-29 21:36:55 -0500

    Se agregaron los 3 tests para el repositorio de pedido El ultimo test aún no funciona de acuerdo a lo esperado

--- Tienda/src/test/java/UnivesidadMagdalena/Tienda/repository/PedidoRepositoryTest.java
+++ Tienda/src/test/java/UnivesidadMagdalena/Tienda/repository/PedidoRepositoryTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,15 +20,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 class PedidoRepositoryTest extends AbstractIntegrationDBTest{
 
     PedidoRepository pedidoRepository;
+    ClienteRepository clienteRepository;
 
     @Autowired
-    public PedidoRepositoryTest(PedidoRepository pedidoRepository) {
+    public PedidoRepositoryTest(PedidoRepository pedidoRepository, ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
         this.pedidoRepository = pedidoRepository;
     }
 
     @BeforeEach
     void setUp() {
 
+        clienteRepository.deleteAll();
         pedidoRepository.deleteAll();
         initMockPedidos();
 
@@ -35,14 +39,13 @@ class PedidoRepositoryTest extends AbstractIntegrationDBTest{
 
     void initMockPedidos() {
         Cliente cliente = Cliente.builder()
-                .id(4554L)
                 .nombre("Jose")
                 .build();
+        clienteRepository.save(cliente);
         Cliente cliente1 = Cliente.builder()
-                .id(35L)
                 .nombre("David")
                 .build();
-
+        clienteRepository.save(cliente1);
         List<ItemPedido> itemPedidoList = new ArrayList<>();
 
 
@@ -69,9 +72,9 @@ class PedidoRepositoryTest extends AbstractIntegrationDBTest{
     void givenAnPedido_whenSave_thenPedidoWithId() {
         //given
         Cliente cliente = Cliente.builder()
-                .id(252424L)
                 .nombre("Jose")
                 .build();
+        clienteRepository.save(cliente);
         List<ItemPedido> itemPedidoList = new ArrayList<>();
         Pedido pedido = Pedido.builder()
                 .cliente(cliente)
@@ -89,7 +92,9 @@ class PedidoRepositoryTest extends AbstractIntegrationDBTest{
 
     @Test
     void findByFechaPedidoBetween() {
-        List<Pedido> pedidoList = pedidoRepository.findByFechaPedidoBetween(LocalDateTime.MIN, LocalDateTime.now());
+        LocalDateTime fechaInicio = LocalDateTime.of(2023, Month.JANUARY, 1, 0, 0, 0);
+        LocalDateTime fechaFin = LocalDateTime.now();
+        List<Pedido> pedidoList = pedidoRepository.findByFechaPedidoBetween(fechaInicio, fechaFin);
 
         assertThat(pedidoList.isEmpty()).isFalse();
 
@@ -97,9 +102,39 @@ class PedidoRepositoryTest extends AbstractIntegrationDBTest{
 
     @Test
     void findByClienteAndStatus() {
+        Cliente cliente = Cliente.builder()
+                .nombre("Pacheco")
+                .build();
+        clienteRepository.save(cliente);
+
+        Pedido pedido = Pedido.builder()
+                .cliente(cliente)
+                .status(Status.PENDIENTE)
+                .build();
+        pedidoRepository.save(pedido);
+        List<Pedido> pedidoList = pedidoRepository.findByClienteAndStatus(cliente, Status.PENDIENTE);
+        assertThat(pedidoList).isNotEmpty();
     }
 
     @Test
     void findPedidosConArticulosByCliente() {
+
+        Cliente cliente = Cliente.builder()
+                .nombre("Pacheco")
+                .build();
+        clienteRepository.save(cliente);
+
+        List<ItemPedido> itemPedidoList = new ArrayList<>();
+
+        Pedido pedido = Pedido.builder()
+                .cliente(cliente)
+                .status(Status.PENDIENTE)
+                .itemPedidos(itemPedidoList)
+                .build();
+        pedidoRepository.save(pedido);
+
+        List<Pedido> pedidoList = pedidoRepository.findPedidosConArticulosByCliente(cliente);
+
+        assertThat(pedidoList).isNotEmpty();
     }
 }
