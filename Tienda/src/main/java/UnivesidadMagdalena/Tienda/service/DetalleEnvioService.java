dd103e858e9bdbaed8fda41f1bc28fd5bb69396b
JosCariDe <jdnarvaez@unimagdalena.edu.co>
2024-03-31 15:19:21 -0500

    Se agregó el controlador para DetalleEnvio Se agregaron metodos que faltaban en la capa de servicios de DetalleEnvio Se implementaron todos los controladores para las entidades Faltan los tests para servicio y para los controladores Ya fue mucho por semana santa

--- Tienda/src/main/java/UnivesidadMagdalena/Tienda/service/DetalleEnvioService.java
+++ Tienda/src/main/java/UnivesidadMagdalena/Tienda/service/DetalleEnvioService.java
@@ -5,10 +5,15 @@ import UnivesidadMagdalena.Tienda.entities.DetalleEnvio;
 import UnivesidadMagdalena.Tienda.enumClass.Status;
 import UnivesidadMagdalena.Tienda.exception.DetalleEnvioNotFoundException;
 
+import java.util.List;
+
 public interface DetalleEnvioService {
     DetalleEnvioDto guardarDetalleEnvio(DetalleEnvioDto detalleEnvioDto);
     DetalleEnvioDto actualizarDetalleEnvio(Long id, DetalleEnvioDto detalleEnvioDto) throws DetalleEnvioNotFoundException;
+    DetalleEnvioDto buscarDetalleEnvioPorId(Long id) throws DetalleEnvioNotFoundException;
+    List<DetalleEnvioDto> getAllDetalleEnvio();
     DetalleEnvioDto buscarDetallesEnvioPorIdPedido(Long idPedido) throws DetalleEnvioNotFoundException;
     DetalleEnvioDto findByTransportadora(String transportadora) throws DetalleEnvioNotFoundException;
     DetalleEnvioDto buscarDetalleenvioPorEstadoDePedido(Status estadoPedido) throws DetalleEnvioNotFoundException;
+    void removerDetalleEnvio(Long id);
 }
--- Tienda/src/main/java/UnivesidadMagdalena/Tienda/service/DetalleEnvioServiceImpl.java
+++ Tienda/src/main/java/UnivesidadMagdalena/Tienda/service/DetalleEnvioServiceImpl.java
@@ -8,6 +8,8 @@ import UnivesidadMagdalena.Tienda.exception.DetalleEnvioNotFoundException;
 import UnivesidadMagdalena.Tienda.repository.DetalleEnvioRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class DetalleEnvioServiceImpl implements DetalleEnvioService {
     private final DetalleEnvioMapper detalleEnvioMapper;
@@ -38,6 +40,21 @@ public class DetalleEnvioServiceImpl implements DetalleEnvioService {
     }
 
     @Override
+    public DetalleEnvioDto buscarDetalleEnvioPorId(Long id) throws DetalleEnvioNotFoundException {
+        DetalleEnvio detalleEnvio =detalleEnvioRepository.findById(id)
+                .orElseThrow(() -> new DetalleEnvioNotFoundException("Detalle de envio no encontrado con id: " + id));
+
+        return detalleEnvioMapper.detalleEnvioToDetalleEnvioDto(detalleEnvio);
+    }
+
+    @Override
+    public List<DetalleEnvioDto> getAllDetalleEnvio() {
+        List<DetalleEnvio> detalleEnvios = detalleEnvioRepository.findAll();
+        if(detalleEnvios.isEmpty()) throw new DetalleEnvioNotFoundException("No hay ningun Detalle de envio en BD");
+        return detalleEnvioMapper.detalleEnviosToDetalleEnviosDto(detalleEnvios);
+    }
+
+    @Override
     public DetalleEnvioDto buscarDetallesEnvioPorIdPedido(Long idPedido) throws DetalleEnvioNotFoundException {
         DetalleEnvio detalleEnvio = detalleEnvioRepository.buscarDetallesEnvioPorIdPedido(idPedido);
         if (detalleEnvio == null) {
@@ -63,5 +80,12 @@ public class DetalleEnvioServiceImpl implements DetalleEnvioService {
             throw new DetalleEnvioNotFoundException("Detalle de envío no encontrado para el estado de pedido: " + estadoPedido);
         }
         return detalleEnvioMapper.detalleEnvioToDetalleEnvioDto(detalleEnvio);
+    }
+
+    @Override
+    public void removerDetalleEnvio(Long id) {
+        DetalleEnvio detalleEnvio = detalleEnvioRepository.findById(id)
+                .orElseThrow(() -> new DetalleEnvioNotFoundException("No se encontró DetalleEnvio a eliminar con la id: " + id));
+        detalleEnvioRepository.delete(detalleEnvio);
     }
 }
