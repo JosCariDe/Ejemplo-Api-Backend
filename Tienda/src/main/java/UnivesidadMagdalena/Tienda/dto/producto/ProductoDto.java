ba5cce4e6d837e2fe9f3ab8c81d6e54dc34ad871
JosCariDe <jdnarvaez@unimagdalena.edu.co>
2024-04-02 15:15:23 -0500

    Se agregó intento de test para controlador No funciona Estoy cansado

--- Tienda/src/main/java/UnivesidadMagdalena/Tienda/dto/producto/ProductoDto.java
+++ Tienda/src/main/java/UnivesidadMagdalena/Tienda/dto/producto/ProductoDto.java
@@ -2,10 +2,12 @@ package UnivesidadMagdalena.Tienda.dto.producto;
 
 import UnivesidadMagdalena.Tienda.dto.itemPedido.ItemPedidoDto;
 import UnivesidadMagdalena.Tienda.dto.pedido.PedidoDto;
+import lombok.Builder;
 
 import java.util.Collections;
 import java.util.List;
 
+@Builder
 public record ProductoDto(Long id,
                           String nombre,
                           Integer price,
--- Tienda/src/test/java/UnivesidadMagdalena/Tienda/TiendaApplicationTests.java
+++ Tienda/src/test/java/UnivesidadMagdalena/Tienda/TiendaApplicationTests.java
@@ -3,7 +3,7 @@ package UnivesidadMagdalena.Tienda;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class TiendaApplicationTests {
 
 	@Test
